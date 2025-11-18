062338fb296848bfa299b183ed78b38e4b460687
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-11-18 10:51:00 +0900

    add:divise導入。サインアップ画面とログイン画面の作成。devise.ja.ymlの作成。ja.ymlの作成。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_11_17_113514) do
+ActiveRecord::Schema.define(version: 2025_11_18_011507) do
 
   create_table "hints", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 2025_11_17_113514) do
     t.string "encrypted_password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "records", "users"
