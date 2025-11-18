5c259c71c42b5c2f20ec6fa94c04179c28fe81c9
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-11-18 23:12:17 +0900

    サインアップ、ログイン、ログアウトできるようにルーティングした。また、ストロングパラメータで名前を保存できるようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # ←redirect先にしたいpathを自分で書く
+  end
 end
