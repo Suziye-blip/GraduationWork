053020df3094dcb6adf2bee02781aa2cd9b30c4d
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-12-07 09:56:46 +0900

    年齢と出身と役職の幅が広すぎて絞る情報として機能していないと感じたので、出身カラムと役職カラムを削除し、住所カラムと学部カラムと学年カラムを追加。また年齢は18~23才までがランダムで出現するように変更。

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_05_062504) do
+ActiveRecord::Schema.define(version: 2025_12_07_002525) do
 
   create_table "hints", force: :cascade do |t|
     t.string "name", null: false
     t.string "gender", null: false
     t.integer "age", null: false
-    t.string "birthplace", null: false
-    t.string "job", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.string "grade"
+    t.string "faculty"
   end
 
   create_table "questions", force: :cascade do |t|
