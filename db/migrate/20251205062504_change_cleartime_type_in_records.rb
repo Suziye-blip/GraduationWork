fdac45e598bfeeff643872333f27d1b1e5f81d62
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-12-08 10:20:31 +0900

    マイグレーションファイルのバージョンを修正。

--- db/migrate/20251205062504_change_cleartime_type_in_records.rb
+++ db/migrate/20251205062504_change_cleartime_type_in_records.rb
@@ -1,4 +1,4 @@
-class ChangeCleartimeTypeInRecords < ActiveRecord::Migration[7.0]
+class ChangeCleartimeTypeInRecords < ActiveRecord::Migration[6.1]
   def up
     change_column :records, :cleartime, :integer, using: 'cleartime::integer'
   end
