6677c1f1920a16a5bc26d92ff48ee2170ddb7877
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-12-08 10:27:46 +0900

    マイグレーションファイルを修正。

--- db/migrate/20251205062504_change_cleartime_type_in_records.rb
+++ db/migrate/20251205062504_change_cleartime_type_in_records.rb
@@ -1,9 +1,5 @@
 class ChangeCleartimeTypeInRecords < ActiveRecord::Migration[6.1]
-  def up
-    change_column :records, :cleartime, :integer, using: 'cleartime::integer'
-  end
-
-  def down
-    change_column :records, :cleartime, :time
+  def change
+    change_column(:records, :cleartime, :integer, using: 'EXTRACT(EPOCH FROM cleartime)::integer')
   end
 end
